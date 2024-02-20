0aef070e6833552f01e36b4ab1fe2a4c79bc3633
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2024-02-21 01:23:39 +0200

    1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 alx-frontend-for-fun to pratice more in frontend
+# My title
+## My title2
+# My title3
+#### My title4
+### My title5
--- markdown2html.py
+++ markdown2html.py
@@ -4,6 +4,28 @@ Markdown to HTML
 """
 import sys
 import os.path
+import re
+
+
+def convert_heading(line):
+    """
+    convert to heading levels
+    """
+    match = re.match(r'^(#+)\s+(.+)', line)
+    if match:
+        h_level = len(match.group(1))
+        h_text = match.group(2)
+        return f'<h{h_level}>{h_text}</h{h_level}>'
+    else:
+        return line
+
+
+def markdown_to_html(markdown_lines):
+    """
+    to convert to html
+    """
+    html_lines = [convert_heading(line) for line in markdown_lines]
+    return '\n'.join(html_lines)
 
 
 if __name__ == '__main__':
@@ -14,4 +36,13 @@ if __name__ == '__main__':
     if not os.path.isfile(sys.argv[1]):
         print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         exit(1)
+
+    with open(sys.argv[1], 'r') as f:
+        markdown_content = f.read().splitlines()
+
+    html_content = markdown_to_html(markdown_content)
+
+    with open(sys.argv[2], 'w') as f:
+        f.write(html_content)
+
     sys.exit(0)
